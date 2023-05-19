d3212fd6892e151bd172afe9173fa728aec05887
hyeseong-dev <hyeseong43@gmail.com>
2023-05-19 17:03:52 +0900

    설정변경

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
-import { join } from "path";
 import express from "express";
-import socketIO from "socket.io";
+import { Server } from "socket.io";
 import logger from "morgan";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 const PORT = 4000;
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
@@ -17,7 +21,7 @@ const handleListening = () =>
 
 const server = app.listen(PORT, handleListening);
 
-const io = socketIO(server);
+const io = Server(server);
 
 let sockets = [];
 
