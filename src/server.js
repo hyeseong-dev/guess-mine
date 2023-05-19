19688c216c5b108e56beb777e155a156c8d47b4c
hyeseong-dev <hyeseong43@gmail.com>
2023-05-19 14:19:32 +0900

    Gulp is Awesome"

--- src/server.js
+++ src/server.js
@@ -1,16 +1,36 @@
 import { join } from "path";
 import express from "express";
 import socketIO from "socket.io";
+import logger from "morgan";
 
 const PORT = 4000;
 const app = express();
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
-app.set("views", join(__dirname, "static"));
+app.use(logger("dev"));
+
+app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => res.render("home"));
 
 const handleListening = () =>
  console.log(`🚀Server running: http://localhost:${PORT}`)
 
-app.listen(PORT, handleListening);
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
+
+let sockets = [];
+
+io.on("connection", socket => {
+    socket.on("newMessage", ({message})=>{
+        socket.broadcast.emit("messageNotif", {
+            message,
+            nickname: socket.nickname || "Anon"
+        })
+    });
+    socket.on("setNickname", ({nickname})=> {
+        socket.nickname = nickname;
+    });
+});
+
--- src/static/index.js
+++ src/static/index.js
@@ -1,1 +1,17 @@
-hello;
+const socket = io("/");
+
+function sendMessage(message){
+    socket.emit("newMessage", {message});
+    console.log(`You: ${message}`);
+}
+
+function handleMessageNotif(data){
+    const { message, nickname } = data;
+    console.log(`${nickname}: ${message}`)
+}
+
+function setNickname(nickname){
+    socket.emit("setNickname", {nickname})
+}
+
+socket.on("messageNotif", handleMessageNotif)
